cb67d9b3f0c89f6ceb160907ecbc449ec16938f4
요한 <icb1696@naver.com>
2025-08-10 00:38:56 +0900

    feat: Q2_C_SQ

--- Data-Structures/Stack_and_Queue/Q2_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q2_C_SQ.c
@@ -113,7 +113,15 @@ int main()
 
 void createStackFromLinkedList(LinkedList *ll, Stack *s)
 {
-    /* add your code here */
+	// stack 비어있지 않으면 먼저 비우기
+	removeAllItemsFromStack(s);
+
+	// stack 생성 
+	ListNode *cur = ll->head;
+	for (int i=0; i < ll->size; i++){
+		push(s, cur->item);
+		cur = cur->next;
+	}
 }
 
 void removeEvenValues(Stack *s)
